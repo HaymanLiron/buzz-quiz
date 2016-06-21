aafac02b7dac5571115c445a51c693de0f10c7b2
Liron <liron.hayman@gmail.com>
2016-06-21 16:58:57 +0300

    small changes

--- BuzzQuiz.js
+++ BuzzQuiz.js
@@ -1,6 +1,44 @@
+var getName = function () {
+    var userName = document.getElementById("nameInput").value;
+    document.getElementById("nameInput").value = "Thanks for your input!";
+};
+
+
 var getAge = function () {
     var obj = document.getElementById("ageRange");
     document.getElementById("printAgeRange").innerHTML = "Your age range is: " + obj.options[obj.selectedIndex].text;
+};
+
+var getGender = function () {
+    if (document.getElementById("Male").checked) {
+        document.getElementById("printGender").innerHTML = "You selected male";
+    } else if (document.getElementById("Female").checked) {
+        document.getElementById("printGender").innerHTML = "You selected female";
+    }
+};
+
+var getDiseases = function () {
+    var obj = document.getElementsByName("diseases");
+    document.getElementById("checklistDiseases").innerHTML = "You have the following diseases: ";
+    for( var i = 0; i<obj.length; i++) {
+        if (obj[i].checked) {
+            document.getElementById("checklistDiseases").innerHTML += obj[i].value + " ";
+        }
+    }
+};
+
+var checkDiseaseText = function () {
+    var userInput = document.getElementById("textInput").value;
+    userInput = userInput.split(",");
+    if (userInput.length > 0 && userInput[0] !== "") {
+        document.getElementById("userDiseases").innerHTML = "You have told us the following diseases: ";
+        for(var i = 0; i < userInput.length; i++) {
+            document.getElementById("userDiseases").innerHTML += userInput[i] + " ";
+        }
+    } else {
+        document.getElementById("userDiseases").innerHTML = "You have not told us any diseases!";
+    }
+
 };
 
 var getAgeRangeScore = function () {
@@ -16,18 +54,18 @@ var getAgeRangeScore = function () {
 
 var getDiseaseListScore = function () {
     var diseaseDic = {
-        "diabetes":10,
-        "heartDisease":20,
-        "HIV":15,
-        "arthritis":5,
-        "asthma":5,
-        "depression":8
+        "Diabetes":10,
+        "Heart Disease":20,
+        "HIV/AIDS":15,
+        "Arthritis":5,
+        "Asthma":5,
+        "Depression":8
     };
     var diseaseScore = 0;
     var obj = document.getElementsByName("diseases");
     for (var i = 0; i < obj.length; i++) {
         if (obj[i].checked) {
-            diseaseScore += diseaseDic[obj[i].id];
+            diseaseScore += diseaseDic[obj[i].value];
         }
     }
     return diseaseScore;
@@ -39,7 +77,9 @@ var getDiseaseFreeTextScore = function () {
     return (userInput.length*10);
 };
 
+
 var calcScore = function () {
+    getDiseases();
     var ageScore = getAgeRangeScore();
     var diseaseScore = getDiseaseListScore();
     var diseaseFreeTextScore = getDiseaseFreeTextScore();
@@ -52,4 +92,13 @@ var calcScore = function () {
     }
 };
 
+
+var calcCheckBoxScore = function () {
+    var blah = document.getElementsByName("diseases");
+    for (var i = 0; i < blah.length; i++) {
+        if (blah[i].checked) {
+            console.log(blah[i].value);
+        }
+    }
+};
 
